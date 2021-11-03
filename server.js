ceafe1250450833577e4b5dfb6848ca26053bf46
Tari <bota.maral@gmail.com>
2021-11-03 09:57:27 -0400

    app get functions

--- server.js
+++ server.js
@@ -11,4 +11,9 @@ const path = require('path');
  app.use(express.json());
  
  // html routes
- 
+ app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
