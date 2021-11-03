3e9fbf53d8db70e66b4b46b9d2c66b0e86893264
Tari <bota.maral@gmail.com>
2021-11-03 10:55:36 -0400

    add store.js file and helper functions

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const { RSA_NO_PADDING } = require("constants");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { addNote } = require("./db/store");
+const store = require("./db/store");
 
 //setup express app
- const app = express();
- const PORT = process.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
- //handle data parsing
- app.use(express.urlencoded({ extended: true}));
- app.use(express.json());
- 
- // html routes
- app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
+//handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// html routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+    store
+    .addNote(req.body).then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+
+app.get("/api/notes", (req, res) => {
+  store
+    .getNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
